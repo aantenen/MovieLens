9753d6089c4fcb274b862388199c6b559d435564
aantenen <andrew.antenen@gmail.com>
2022-03-09 23:33:21 -0600

    Add adjusted effect models

--- getData.R
+++ getData.R
@@ -8,6 +8,7 @@
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+if(!require(ggridges)) install.packages("ggridges", repos = "http://cran.us.r-project.org")
 
 library(tidyverse)
 library(caret)
@@ -16,7 +17,7 @@ library(ggplot2)
 library(randomForest)
 library(lubridate)
 library(stringr)
-
+library(ggridges)
 
 # MovieLens 10M dataset:
 # https://grouplens.org/datasets/movielens/10m/
--- movielens.R
+++ movielens.R
@@ -13,20 +13,21 @@ test_setEdx<-test_setEdx%>%
   semi_join(train_setEdx,by='movieId')%>%
   semi_join(train_setEdx,by='userId')
 
-#setup small test dataset, comment out after completed
-minEdx<-slice_sample(edx,n=10000)
-minTest_index<-createDataPartition(minEdx$rating,times = 1,p=.2,list=FALSE)
-minTest_setEdx<-minEdx[minTest_index]
-minTrain_setEdx<-minEdx[-minTest_index]
+# #setup small test dataset, comment out after completed
+# minEdx<-slice_sample(edx,n=100000)
+# minTest_index<-createDataPartition(minEdx$rating,times = 1,p=.2,list=FALSE)
+# minTest_setEdx<-minEdx[minTest_index]
+# minTrain_setEdx<-minEdx[-minTest_index]
+# 
+# 
+# minTest_setEdx<-minTest_setEdx%>%
+#   semi_join(minTrain_setEdx,by='movieId')%>%
+#   semi_join(minTrain_setEdx,by='userId')
+# test_setEdx<-minTest_setEdx
+# train_setEdx<-minTrain_setEdx
 
-
-minTest_setEdx<-minTest_setEdx%>%
-  semi_join(minTrain_setEdx,by='movieId')%>%
-  semi_join(minTrain_setEdx,by='userId')
-test_setEdx<-minTest_setEdx
-train_setEdx<-minTrain_setEdx
-
-#add year and age values to test set
+#add year and age values to sets
+train_setEdx<-train_setEdx%>%mutate(year=str_sub(title,-5,-2),age=year(as_datetime(timestamp))-as.numeric(str_sub(title,-5,-2)))
 test_setEdx<-test_setEdx%>%mutate(year=str_sub(title,-5,-2),age=year(as_datetime(timestamp))-as.numeric(str_sub(title,-5,-2)))
 
 ###TO DO check ranges of values: User, Movie, Score
@@ -41,25 +42,25 @@ overallAvgRating<-mean(test_setEdx$rating)
 naiveModelRMSE<-RMSE(test_setEdx$rating,overallAvgRating)
 
 #build dataframes for testing groupings against averages
-movieReviewCnt <-train_setEdx%>%mutate(group=movieId)%>%group_by(group,title)%>%
-  summarise(avg=mean(rating),cnt=n(),sd=sd(rating),movieEffect=mean(rating-overallAvgRating))%>%
+movieReviewCnt <-train_setEdx%>%group_by(movieId,title)%>%
+  summarise(avg=mean(rating),cnt=n(),sd=sd(rating),effect=mean(rating-overallAvgRating))%>%
   mutate(min=avg-sd,max=avg+sd)
   movieReviewCnt <-movieReviewCnt[,c(1,3:8,2)]
   
-userReviewCnt <-train_setEdx%>%mutate(group=userId)%>%group_by(group)%>%
-  summarize(avg=mean(rating),cnt=n(),sd=sd(rating),userEffect=mean(rating-overallAvgRating))%>%
+userReviewCnt <-train_setEdx%>%group_by(userId)%>%
+  summarize(avg=mean(rating),cnt=n(),sd=sd(rating),effect=mean(rating-overallAvgRating))%>%
   mutate(min=avg-sd,max=avg+sd)
 
-genreReviewCnt<-train_setEdx%>%mutate(group=genres)%>%group_by(group)%>%
-  summarise(avg=mean(rating),cnt=n(),sd=sd(rating),genreEffect=mean(rating-overallAvgRating))%>%
+genreReviewCnt<-train_setEdx%>%group_by(genres)%>%
+  summarise(avg=mean(rating),cnt=n(),sd=sd(rating),effect=mean(rating-overallAvgRating))%>%
   mutate(min=avg-sd,max=avg+sd)
 
-yearReviewCnt<-train_setEdx%>%mutate(group=str_sub(title,-5,-2))%>%group_by(group)%>%
-  summarize(avg=mean(rating),cnt=n(),sd=sd(rating),yearEffect=mean(rating-overallAvgRating))%>%
+yearReviewCnt<-train_setEdx%>%mutate(year=str_sub(title,-5,-2))%>%group_by(year)%>%
+  summarize(avg=mean(rating),cnt=n(),sd=sd(rating),effect=mean(rating-overallAvgRating))%>%
   mutate(min=avg-sd,max=avg+sd)
 
-movieAgeReviewCnt<-train_setEdx%>%mutate(group=year(as_datetime(timestamp))-as.numeric(str_sub(title,-5,-2)))%>%group_by(group)%>%
-  summarize(avg=mean(rating),cnt=n(),sd=sd(rating),yearEffect=mean(rating-overallAvgRating))%>%
+movieAgeReviewCnt<-train_setEdx%>%mutate(age=year(as_datetime(timestamp))-as.numeric(str_sub(title,-5,-2)))%>%group_by(age)%>%
+  summarize(avg=mean(rating),cnt=n(),sd=sd(rating),effect=mean(rating-overallAvgRating))%>%
   mutate(min=avg-sd,max=avg+sd)
 
 avgRatingChart<-function(x){
@@ -76,39 +77,78 @@ dfList=list(movieReviewCnt,userReviewCnt,genreReviewCnt,yearReviewCnt,movieAgeRe
 ratingCharts<-lapply(dfList, avgRatingChart)
 #cntCharts<-lapply(dfList,cntChart)
 
-#test avg score for each category
+#test individual effects for each category
 
-guessAvg<- function(x,y){
-  #x - pass list of group from test set
-  #y - pass training data frame grouped by tested attribute
-  #y<-as.matrix(y)
-  guess<-y[which(y[,1]==x),2]
+# guessAvg<- function(x,y){
+#   #x - pass list of group from test set
+#   #y - pass training data frame grouped by tested attribute
+#   #y<-as.matrix(y)
+#   guess<-y[which(y[,1]==x),2]
+#   as.numeric(guess)
+#   
+#   
+# }
+
+dimensionEffect<-function(x,y){
+  guess<-overallAvgRating+test_setEdx%>%left_join(x,by=y)%>%pull(effect)
   as.numeric(guess)
-}
+  }
 
-print(Sys.time())
-movieModel<-data.frame(id=test_setEdx$movieId,guess=sapply(X=test_setEdx$movieId,FUN=guessAvg,y=movieReviewCnt))
-movieModelRMSE<-RMSE(movieModel$guess,test_setEdx$rating)
-print(Sys.time())
-userModel<-data.frame(id=test_setEdx$userId,guess=sapply(test_setEdx$userId,guessAvg,y=userReviewCnt))
-userModelRMSE<-RMSE(userModel$guess,test_setEdx$rating)
-  
-genreModel<-data.frame(id=test_setEdx$genres,guess=sapply(test_setEdx$genres,guessAvg,y=genreReviewCnt))
-genreModelRMSE<-RMSE(genreModel$guess,test_setEdx$rating)
-
-yearModel<-data.frame(id=test_setEdx$year,guess=sapply(test_setEdx$year,guessAvg,y=yearReviewCnt))
-yearModelRMSE<-RMSE(yearModel$guess,test_setEdx$rating)
-
-ageModel <-data.frame(id=test_setEdx$age,guess=sapply(test_setEdx$age,guessAvg,y=movieAgeReviewCnt))
-ageModelRMSE<-RMSE(ageModel$guess,test_setEdx$rating)
+# movieEffectModel<-data.frame(guess=dimensionEffect(x=movieReviewCnt,y='movieId'))
+# movieEffectRMSE<-RMSE(movieEffectModel$guess,test_setEdx$rating)
+# 
+# userEffectModel<-data.frame(guess=dimensionEffect(x=userReviewCnt,y='userId'))
+# userEffectRMSE<-RMSE(userEffectModel$guess,test_setEdx$rating)
+# 
+# genreEffectModel<-data.frame(guess=dimensionEffect(x=genreReviewCnt,y='genres'))
+# genreEffectRMSE<-RMSE(genreEffectModel$guess,test_setEdx$rating)
+# 
+# yearEffectModel<-data.frame(guess=dimensionEffect(x=yearReviewCnt,y='year'))
+# yearEffectRMSE<-RMSE(yearEffectModel$guess,test_setEdx$rating)
+# 
+# ageEffectModel<-data.frame(guess=dimensionEffect(x=movieAgeReviewCnt,y='age'))
+# ageEffectRMSE<-RMSE(ageEffectModel$guess,test_setEdx$rating)
+#  #mModelS<-Sys.time()
+# movieModel<-data.frame(id=test_setEdx$movieId,guess=sapply(X=test_setEdx$movieId,FUN=guessAvg,y=movieReviewCnt))
+# movieModelRMSE<-RMSE(movieModel$guess,test_setEdx$rating)
+# #mModelE<-Sys.time()
+# #uModelS<-Sys.time()
+# userModel<-data.frame(id=test_setEdx$userId,guess=sapply(X=test_setEdx$userId,FUN=guessAvg,y=userReviewCnt))
+# userModelRMSE<-RMSE(userModel$guess,test_setEdx$rating)
+# #uModelE<-Sys.time()  
+# genreModel<-data.frame(id=test_setEdx$genres,guess=sapply(test_setEdx$genres,guessAvg,y=genreReviewCnt))
+# genreModelRMSE<-RMSE(genreModel$guess,test_setEdx$rating)
+# 
+# yearModel<-data.frame(id=test_setEdx$year,guess=sapply(test_setEdx$year,guessAvg,y=yearReviewCnt))
+# yearModelRMSE<-RMSE(yearModel$guess,test_setEdx$rating)
+# 
+# ageModel <-data.frame(id=test_setEdx$age,guess=sapply(test_setEdx$age,guessAvg,y=movieAgeReviewCnt))
+# ageModelRMSE<-RMSE(ageModel$guess,test_setEdx$rating)
 
 #plot model RMSE
 #To Do: improve plot, create plot function
-rmseList<-c(naiveModelRMSE,movieModelRMSE,userModelRMSE,genreModelRMSE,yearModelRMSE,ageModelRMSE)
+rmseList<-c(naiveModelRMSE,movieEffectRMSE,userEffectRMSE,genreEffectRMSE,yearEffectRMSE,ageEffectRMSE)
 plot(rmseList)
 
-#movie effect, user effect etc.
-movieEffectModel <- overallAvgRating + test_setEdx%>%
-  left_join(movieReviewCnt,by='group')%>%pull(movieEffect)
-movieEffectModelRMSE<-RMSE(movieEffectModel,test_setEdx$rating)
+#create ridgeline visual with errors from each model
+
+#combine dimension effects
+movieEffect<-data.frame(movieId=movieReviewCnt$movieId,effect=movieReviewCnt$effect)
+userAdjEffect<-train_setEdx%>%left_join(movieReviewCnt,by='movieId')%>%group_by(userId)%>%
+  summarize(userAdjEffect = mean(rating - overallAvgRating-effect))
+userAdjModel<-test_setEdx%>%left_join(movieEffect,by='movieId')%>%left_join(userAdjEffect,by='userId')%>%
+  mutate(guess=overallAvgRating+effect+userAdjEffect)%>%pull(guess)
+userAdjEffectRMSE<-RMSE(userAdjModel,test_setEdx$rating)
+#genreAdjEffect<-train_setEdx%>%left_join(movieEffect,by='movieId')%>%
+# group_by(genres)%>%summarize(genreAdjEffect=rating-overallAvgRating-effect)
+
+
+#genreAdjModel<-test_setEdx%>%left_join(movieEffect,by='movieId')%>%
+#  left_join(genreAdjEffect,by='genres')%>%mutate(guess=overallAvgRating+effect+genreAdjEffect)
+yearAdjEffect<-train_setEdx%>%left_join(movieReviewCnt,by='movieId')%>%left_join(userAdjEffect,by='userId')%>%group_by(year)%>%
+  summarise(yearAdjEffect=mean(rating-overallAvgRating-effect-userAdjEffect))
+yearAdjModel<-test_setEdx%>%left_join(movieReviewCnt,by='movieId')%>%
+  left_join(userAdjEffect,by='userId')%>%left_join(yearAdjEffect,by='year')%>%
+  mutate(guess=overallAvgRating+effect+userAdjEffect+yearAdjEffect)%>%pull(guess)
+yearAdjRMSE<-RMSE(yearAdjModel,test_setEdx$rating)
 
